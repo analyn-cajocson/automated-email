1a81aaa6c818ffd22eeacb430d0cf99a0da68803
Analyn Cajocson <analyncajocson@ymail.com>
2018-05-01 17:41:50 +0800

    created automated email

---
+++ email.rb
@@ -0,0 +1,30 @@
+require 'mail'
+
+class Email
+	attr_reader :recipient
+
+	def self.execute
+		options = { :address              => "smtp.gmail.com",
+	            :port                 => 587,
+	            :domain               => 'gmail.com',
+	            :user_name            => '',
+	            :password             => '',
+	            :authentication       => 'plain',
+	            :enable_starttls_auto => true  }
+		Mail.defaults do
+	  delivery_method :smtp, options
+		end
+
+		Mail.deliver do
+		       to []
+		       bcc []
+		     from 'analyncajocson@gmail.com'
+		  subject 'BACKPAY'
+		     body File.read('email_template.html')
+		end
+		
+	end
+end
+
+send_email = Email.execute
+
