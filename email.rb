1cb1e9cfb726582fa198af89736f367e173ee15e
Analyn Cajocson <analyncajocson@ymail.com>
2018-05-01 17:52:54 +0800

    coding standard

--- email.rb
+++ email.rb
@@ -1,30 +1,28 @@
 require 'mail'
 
 class Email
-	attr_reader :recipient
+  attr_reader :recipient
 
-	def self.execute
-		options = { :address            => "smtp.gmail.com",
-	            :port                 => 587,
-	            :domain               => 'gmail.com',
-	            :user_name            => '',
-	            :password             => '',
-	            :authentication       => 'plain',
-	            :enable_starttls_auto => true  }
-		Mail.defaults do
-	  	delivery_method :smtp, options
-		end
+  def self.execute
+    options = { address: 'smtp.gmail.com',
+                port: 587,
+                domain: 'gmail.com',
+                user_name: '',
+                password: '',
+                authentication: 'plain',
+                enable_starttls_auto: true }
+    Mail.defaults do
+      delivery_method :smtp, options
+    end
 
-		Mail.deliver do
-			to []
-		  bcc []
-		  from ''
-		  subject 'BACKPAY'
-		  body File.read('email_template.html')
-		end
-		
-	end
+    Mail.deliver do
+      to []
+      bcc []
+      from ''
+      subject 'BACKPAY'
+      body File.read('email_template.html')
+    end
+  end
 end
 
 send_email = Email.execute
-
